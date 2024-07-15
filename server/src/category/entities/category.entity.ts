1346b722f3931e42873f7052fb978e5484c9a963
GermanPaul <german.kalmykov@mail.ru>
2024-07-15 11:01:40 +0300

    aaa

--- server/src/category/entities/category.entity.ts
+++ server/src/category/entities/category.entity.ts
@@ -1,1 +1,10 @@
-export class Category {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Category {
+  @PrimaryGeneratedColumn({ name: 'category_id' })
+  id: number;
+
+  @Column()
+  title: string;
+}
--- server/src/user/entities/user.entity.ts
+++ server/src/user/entities/user.entity.ts
@@ -1,4 +1,24 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
   email: string;
+
+  @Column()
   password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @CreateDateColumn()
+  updatedAt: Date;
 }
