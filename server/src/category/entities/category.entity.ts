c6ce51ad31099d5b822a68df94f5ffbfcaa4a1cc
GermanPaul <german.kalmykov@mail.ru>
2024-07-26 09:51:50 +0300

    feat: add entity for user, category and transaction

--- server/src/category/entities/category.entity.ts
+++ server/src/category/entities/category.entity.ts
@@ -1,4 +1,6 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Transaction } from 'src/transaction/entities/transaction.entity';
+import { User } from 'src/user/entities/user.entity';
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity()
 export class Category {
@@ -7,4 +9,17 @@ export class Category {
 
   @Column()
   title: string;
+
+  @ManyToOne(() => User, (user) => user.categories)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @OneToMany(() => Transaction, (transaction) => transaction.category)
+  transactions: Transaction[]
+
+  @CreateDateColumn()
+  createAt: Date;
+
+  @UpdateDateColumn()
+  updateAt: Date;
 }
